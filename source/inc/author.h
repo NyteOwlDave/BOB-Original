649263116bf2009858921927b0f1d25110367d91
NyteOwlDave <nyteowl64@gmail.com>
2021-07-29 00:04:44 -0500

    Created source folder

---
+++ source/inc/author.h
@@ -0,0 +1,23 @@
+
+/*
+   Much of this code comes from software included in the book
+   Photorealism and Ray Tracing in C
+   ISBN 1-55851-247-0
+   Christpher D. Watkins
+   Stephen B. Coy
+   Mark Finlay
+
+   Copyright Algorithm, Inc.
+   
+   That said, extensive bug fixes, formatting and enhancements
+   were made by:
+
+   Dave Wellsted
+   NyteOwl Consulting
+   https://fb.me/nyteowl64
+
+   Anyone wishing to use the code must ensure that the original
+   authors permit such usage. As for my part, I donate my own
+   efforts to the public domain.
+
+*/
